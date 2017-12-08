faf633d7f1b74524aa84f561bf4d46c2dc3767c8
Krzysztof Stanisławek <krzysztof.stanislawek@gemius.com>
2017-12-08 23:16:10 +0100

    test reorganization

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,1 @@
-#!/bin/bash +x
-
-./debug.e 0.csv < 0.in 2> /dev/null | diff 0.out -
-./debug.e 1.csv < 1.in
-./debug.e 2.csv
+../test_data/test.sh
---
+++ test_data/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+(
+cd $(dirname $( readlink -f $0))
+tests="10x2 1x4 2x2 syntax_errors semantic_errors"
+for t in $tests; do
+    echo $t
+    if [ "$1" == "apply" ]; then
+        ./debug.e $t.csv < $t.in 2> /dev/null > $t.out
+    else
+        ./debug.e $t.csv < $t.in 2> /dev/null | diff $t.out -
+    fi
+done
+)
